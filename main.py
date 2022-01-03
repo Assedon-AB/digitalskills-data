e489a922d896969b4d085292ad432d0398134037
Elias Juremalm <eliass.juremalm@gmail.com>
2022-01-03 13:04:51 +0100

    add tasks/planning for main file

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 def main():
+    # Filtrera annonser. -> Extract ad info file
+    # Extrahera kompetens, yrke och mjukavärden
+        # Extrahera kompetenser med -> extract-skills
+        # Extrahera yrke och mjukavärden med -> enrich_ads.py
+    # Bygg relationer mellan kompetenser, yrke och mjukvärden. -> relationship-builder.py
+    # Skapa en prognos av yrken och kompetenser -> skills-prediction.py
+    # Spara ner slutresultatet i en databas.
+        # Fil som använder sig av POST routes i API
     pass
 
 if __name__ == "__main__":
--- save-predictions.py
+++
@@ -1,9 +0,0 @@
-""" Takes prediction data and saves to databse """
-
-def get_predictions():
-    """ Gets skill predictions """
-    pass
-
-def save_predictions():
-    """ Saves all predictions in database """
-    pass
