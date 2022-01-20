1f34302fdc582305e19e63eaa3f7e7ecb08b3f2e
Elias Juremalm <eliass.juremalm@gmail.com>
2022-01-20 14:16:27 +0100

    change from looking for unique words to broad regex match against definition words in whitelist

--- extract_skills.py
+++ extract_skills.py
@@ -8,7 +8,7 @@ from spinner import Spinner
 
 def get_ads_data():
     """ Gets ads data """
-    years = ["2006","2007", "2008", "2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2019","2020","2021"]
+    years = ["2006","2007", "2008", ]#"2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2019","2020","2021"]
     ads = []
     for year in years:
         ad = json.load(open(f"./ads/{year}.json", "r"))
@@ -19,7 +19,6 @@ def get_whitelist():
     """ Reads in whitelist """
     file = open("./whitelist.csv")
     csvreader = csv.reader(file)
-    header = next(csvreader)
     rows = []
     for row in csvreader:
         rows.append(row)
@@ -46,8 +45,8 @@ def extract_skills(ads):
 
     skills_data = {}
     for row in whitelist:
-        skills_data[row[2]] = {"series": [], "adIds": [], "subgroup": row[3], "maingroup": row[4], "employers": {}}
-        skills_data[row[2]]["series"] = pd.Series([0] * num, index=index)
+        skills_data[row[1]] = {"series": [], "adIds": [], "subgroup": row[2], "maingroup": row[3], "employers": {}}
+        skills_data[row[1]]["series"] = pd.Series([0] * num, index=index)
 
     print(f"> Extracting skills from ads data")
     spinner = Spinner()
@@ -57,18 +56,18 @@ def extract_skills(ads):
             if len(ad["date"]) > 9:
                 date = ad["date"].split("T")[0].split(" ")[0]
                 for row in whitelist:
-                    skill = row[0]
+                    skill_regex = row[0]
                 
-                    p = re.compile('[-_\\s^]' + re.escape(skill) + '[\\s$-_]')
+                    p = re.compile(skill_regex)
                     if bool(p.search(ad["doc_text"].lower())):
-                        skills_data[row[2]]["series"][date] += 1
-                        skills_data[row[2]]["adIds"].append(adId)
+                        skills_data[row[1]]["series"][date] += 1
+                        skills_data[row[1]]["adIds"].append(adId)
 
                         if "employer" in ad:
-                            if ad["employer"] in skills_data[row[2]]["employers"]:
-                                skills_data[row[2]]["employers"][ad["employer"]] += 1
+                            if ad["employer"] in skills_data[row[1]]["employers"]:
+                                skills_data[row[1]]["employers"][ad["employer"]] += 1
                             else:
-                                skills_data[row[2]]["employers"][ad["employer"]] = 1
+                                skills_data[row[1]]["employers"][ad["employer"]] = 1
 
 
         save_skills_data_to_json(skills_data)
---
+++ whitelist-regexer.py
@@ -0,0 +1,35 @@
+import csv
+import re
+""" Reads in whitelist """
+file = open("./whitelist.csv")
+csvreader = csv.reader(file)
+header = next(csvreader)
+rows = []
+for row in csvreader:
+    rows.append(row)
+file.close()
+
+skills = {}
+for row in rows:
+    if row[2] in skills:
+        skills[row[2]]["words"].append(row[0])
+    else:
+        skills[row[2]] = {}
+        skills[row[2]]["words"] = [row[0]]
+        skills[row[2]]["subgroup"] = row[3]
+        skills[row[2]]["maingroup"] = row[4]
+
+new_rows = []
+for skill in skills.keys():
+    new_rows.append([
+        f'[\\s_-]({re.escape("|".join(skills[skill]["words"]))})[\\.\\s_-]',
+        skill,
+        skills[skill]["subgroup"],
+        skills[skill]["maingroup"]
+    ])
+
+
+file = open("./whitelist-regex.csv", "w")
+csvwriter = csv.writer(file)
+csvwriter.writerows(new_rows)
+file.close()
