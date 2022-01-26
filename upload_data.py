60e2c0854f6f23694078c5add7c4bd9078672aff
Elias Juremalm <eliass.juremalm@gmail.com>
2022-01-26 18:36:48 +0100

    add simple upload to api script

--- upload_data.py
+++ upload_data.py
@@ -7,9 +7,20 @@ def upload_data(data, collection="kompetenser" ):
     if not API_URL:
         API_URL = "http://localhost:8080/api/v1/"
 
+    API_KEY = os.environ.get("API_KEY")
+
     for d in data:
-        r = requests.post(API_URL+collection, d)
+        r = requests.post(API_URL+collection, d, headers={
+                "x-api-key": API_KEY,
+                }
+            )
+        print(r.status_code)
 
 if __name__ == "__main__":
-    data = []
+    data = [
+            {
+                "name": "React",
+                "description": "react framework"
+                }
+            ]
     upload_data(data)
